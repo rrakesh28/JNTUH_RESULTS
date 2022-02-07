c06fc1abe999fd5fe7a8f11646bccc7aed00a2e3
ThilakReddyy <thilakreddy19102000@gmail.com>
2022-02-08 04:35:31 +0530

    First commit

--- jntuhresults/settings.py
+++ jntuhresults/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "Static")
 ]
 print(STATICFILES_DIRS)
 
