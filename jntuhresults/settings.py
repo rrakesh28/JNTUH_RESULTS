08c4a20c207f5854c9947d0327966cb8092a9998
ThilakReddyy <thilakreddy19102000@gmail.com>
2022-02-08 04:14:32 +0530

    First commit

--- jntuhresults/settings.py
+++ jntuhresults/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_wn1noib8wjb%5ui*w-scymn=dhwunn(dzj@woq7437uw%)$p^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
