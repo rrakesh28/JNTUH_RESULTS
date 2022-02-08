ae3182f79ac842d2c385bab25a1174cce2d544e4
THILAK REDDY <64121161+ThilakReddyy@users.noreply.github.com>
2022-02-09 00:59:53 +0530

    Update views.py

--- jntuhresults/views.py
+++ jntuhresults/views.py
@@ -56,6 +56,6 @@ def oneroll(request):
 def gettingurl(request,htno,code):
     deta={}
     deta=Search_by_Roll_number.the_loaderi(htno,code)
-    if(!bool(deta[code])):
+    if(bool(deta[code])==False):
         return HttpResponse("")
     return render(request,'snippet.html',{'deta':deta})
